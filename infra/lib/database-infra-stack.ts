b9c886afa73bc72dc99fa38c5fc618d7d1d71a0c
Shankar <psgganesh@gmail.com>
2023-08-21 16:59:48 +1000

    adding public

--- infra/lib/database-infra-stack.ts
+++ infra/lib/database-infra-stack.ts
@@ -18,6 +18,11 @@ export class DatabaseInfraStack extends cdk.Stack {
       subnetConfiguration: [
         {
           cidrMask: 24,
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
           name: 'private',
           subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
         }
