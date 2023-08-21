9121a60e7b3cdad0ca16c2feb067adce13dd8b5f
Shankar <psgganesh@gmail.com>
2023-08-21 18:27:02 +1000

    Adding stack boileplate

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,21 +2,14 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DatabaseInfraStack } from '../lib/database-infra-stack';
+import { PeeringInfraStack } from '../lib/peering-infra-stack';
 
 const app = new cdk.App();
 
-new DatabaseInfraStack(app, 'mysqldatabase', {
-    /* If you don't specify 'env', this stack will be environment-agnostic.
-     * Account/Region-dependent features and context lookups will not work,
-     * but a single synthesized template can be deployed anywhere. */
-  
-    /* Uncomment the next line to specialize this stack for the AWS Account
-     * and Region that are implied by the current CLI configuration. */
+const environment = {
     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  
-    /* Uncomment the next line if you know exactly what Account and Region you
-     * want to deploy the stack to. */
-    // env: { account: '123456789012', region: 'us-east-1' },
-  
-    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+}
+
+new DatabaseInfraStack(app, 'mysqldatabase', environment);
+
+new PeeringInfraStack(app, 'dbnetwork', environment);
