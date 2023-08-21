8d661573db7ff9ff1bf5758291a78e2d799be668
Shankar <psgganesh@gmail.com>
2023-08-21 12:17:08 +1000

    Secrets manager and publicly accesible on cidr

--- infra/lib/database-infra-stack.ts
+++ infra/lib/database-infra-stack.ts
@@ -2,29 +2,61 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 
 export class DatabaseInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create a new VPC (or use an existing one)
-    const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 3 });
-
-    // Define the MySQL database
-    const database = new rds.DatabaseInstance(this, 'MySQLDatabase', {
-      engine: rds.DatabaseInstanceEngine.mysql({
-        version: rds.MysqlEngineVersion.VER_8_0_33,
-      }),
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
-      vpc,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // ONLY FOR DEV/TEST
-      deletionProtection: false, // ONLY FOR DEV/TEST
+    // You might want to fetch the default VPC in which Cloud9 is launched
+    const vpc = ec2.Vpc.fromLookup(this, 'DefaultVpc', {
+      isDefault: true
     });
 
+    // Cloud9 EC2 CIDR block - you would obtain this dynamically, 
+    // but for the sake of this example, let's assume a placeholder CIDR
+    const cloud9Cidr = this.node.tryGetContext('cloud9Cidr') || '0.0.0.0/0';
+
+    // Create a secret for RDS username and password
+    const mysqlUsername = 'dbadmin';
+    const mysqlSecret = new secretsmanager.Secret(this, 'DB Admin Credentials', {
+      secretName: 'DB-Admin-Credential',
+      description: 'Mysql Database Crendentials',
+      generateSecretString: {
+        excludeCharacters: "\"@/\\ '",
+        generateStringKey: 'password',
+        passwordLength: 30,
+        secretStringTemplate: JSON.stringify({username: mysqlUsername}),
+      },
+    });
+
+    const mysqlCredentials = rds.Credentials.fromSecret(
+      mysqlSecret,
+      mysqlUsername,
+    );
+
+    const database = new rds.DatabaseInstance(this, 'Database', {
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_0_21,
+      }),
+      vpc: vpc,
+      publiclyAccessible: true,
+      credentials: mysqlCredentials,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Careful! This means the database is destroyed when the stack is deleted
+      deleteAutomatedBackups: true,
+    });
+
+    // Allow connection only from Cloud9 CIDR
+    database.connections.allowFrom(ec2.Peer.ipv4(cloud9Cidr), ec2.Port.tcp(3306));
+
     new cdk.CfnOutput(this, 'DatabaseEndpoint', {
-        value: database.dbInstanceEndpointAddress,
-        description: 'The endpoint address of the RDS instance',
-        exportName: 'RDSInstanceEndpoint'
+      value: database.dbInstanceEndpointAddress,
+      description: 'The RDS MySQL instance endpoint',
+    });
+
+    new cdk.CfnOutput(this, 'DatabaseSecurityGroupId', {
+      value: database.connections.securityGroups[0].securityGroupId,
+      description: 'The security group ID of the RDS MySQL instance',
     });
   }
 }
