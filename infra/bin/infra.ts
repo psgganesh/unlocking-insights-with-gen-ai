4a215eb6f717928388eeae7570700897017b82ea
Shankar <psgganesh@gmail.com>
2023-08-23 10:11:29 +1000

    Update commands to identify missing env vars

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -11,25 +11,16 @@ import { Cloud9RtbStack } from '../lib/cloud9-rtb-update-stack';
 let REGION : string 
 let ACCOUNT_ID : string
 
-if(!process.env.REGION || !process.env.ACCOUNT_ID){
-  console.log("Missing Environment Varialbes for REGION ACCOUNT_ID!")
-  //import-vpc-guide.md
-  //or run below command
-  //chmod +x run ../run.sh
-  //../run.sh
+if(!process.env.REGION || !process.env.ACCOUNT_ID) {
+  console.log("Missing environment variables for REGION and ACCOUNT_ID!")
   process.exit(1);
-}else{
+} else {
   console.log("Ready to Go!")
   REGION = process.env.REGION
   ACCOUNT_ID = process.env.ACCOUNT_ID
 }
 
 const app = new cdk.App();
-
-// const environment = {
-//     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-// }
-
 const rdsStack = new DatabaseInfraStack(app, 'mysqldatabase', {});
 const defaultVpc = new ImportedDefaultVpcStack(app, 'default-vpc', {
     env: {
@@ -38,8 +29,6 @@ const defaultVpc = new ImportedDefaultVpcStack(app, 'default-vpc', {
     }
 })
 
-// const cloud9VpcId = app.node.tryGetContext('cloud9Cidr') || '172.31.0.0/16';
-
 const peeringDefault = new PeeringInfraStack(app, 'dbnetwork', {
     PEER_OWNER_ID: ACCOUNT_ID,
     REGION: REGION,
@@ -47,14 +36,16 @@ const peeringDefault = new PeeringInfraStack(app, 'dbnetwork', {
     PEER_VPC_ID: rdsStack.vpc.vpcId,
     
 });
+
 new RdsRtbStack(app, 'rds-cloud9-rtb',{
     PEERING_CONNECTION: peeringDefault.peering,
     RDS_VPC: rdsStack.vpc,
     DEST_VPC_CIDR: defaultVpc.vpc.vpcCidrBlock
-})
+});
+
 new Cloud9RtbStack(app, 'cloud9-rds-rtb',{
     CLOUD9_VPC: defaultVpc.vpc,
     DEST_VPC_CIDR: rdsStack.vpc.vpcCidrBlock,
     PEERING_CONNECTION: peeringDefault.peering,
     VPC_DEFAULT: true
-})
+});
